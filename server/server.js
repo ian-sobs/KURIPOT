9a0c9ae3a3407db2dee35e70d8ddc2b8bda7506d
ian <ian.sobrecaray@gmail.com>
2024-11-03 05:55:13 +0800

    Started integrating the project with the PostegreSQL database

--- server/server.js
+++ server/server.js
@@ -4,10 +4,26 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+const { Sequelize } = require('sequelize');
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'postgres' 
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+
