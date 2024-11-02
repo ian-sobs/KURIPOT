34a967170850cea08cfb7bb959b796c704c21fa6
ian <ian.sobrecaray@gmail.com>
2024-11-03 06:13:43 +0800

    Wrapped the try catch code block in an async function

--- server/server.js
+++ server/server.js
@@ -11,19 +11,24 @@ const PORT = process.env.PORT || 5000;
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('Kuripot', 'postgres', 'falgor123', {
   host: 'localhost',
+  port: 5432,
   dialect: 'postgres' 
 });
 
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
 
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+startServer();
 
