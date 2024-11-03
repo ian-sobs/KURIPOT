9a074fa2c2cd85a8fa785d797f441936ed955fc3
ian <ian.sobrecaray@gmail.com>
2024-11-03 18:24:41 +0800

    Implemented transactions sequelize model

---
+++ server/models/Transaction.js
@@ -0,0 +1,37 @@
+const {DataTypes } = require('sequelize');
+const {sequelize, connectDB} = require('../config/db');
+
+const Transaction = sequelize.define(
+  'Transaction',
+  {
+    // Model attributes are defined here
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    amount: {
+      type: DataTypes.DECIMAL(16,2),
+      allowNull: false,
+    },
+    // have to implement account_id foreign key
+    date:{
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    // have to implement category_id foreign key
+    // have to implement from_account_id foreign key
+    // have to implement to_account_id foreign key
+    note:{
+        type: DataTypes.STRING,
+    }
+    // have to implement recurr_id foreign key
+  },
+  {
+    // Other model options go here
+    tableName: 'transactions',
+  },
+);
+
+module.exports = Transaction;
