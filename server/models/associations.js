117ad4c78a2452aca294aafd17e36eeb142a9812
ian <ian.sobrecaray@gmail.com>
2024-11-06 07:57:50 +0800

    Associations between models, or tables, have been set up

---
+++ server/models/associations.js
@@ -0,0 +1,121 @@
+const sequelize = require('../config/db')
+
+const setupAssociations = () => {
+    const { Account, Budget, BudgetCategory, Category, RecurrTransacGrp, Transaction, User } = sequelize.models;
+  
+    //One-to-Many relationships between sequelize models defautls to CASCADE ON UPDATE
+    User.hasMany(Account, {
+        foreignKey: {
+            name: 'user_id',
+            
+            allowNull: false
+        },
+        onDelete: 'CASCADE',
+    });
+ 
+    User.hasMany(Budget, {
+        foreignKey: {
+            name: 'user_id',
+            
+            allowNull: false
+        },
+        onDelete: 'CASCADE',
+    });
+
+    User.hasMany(RecurrTransacGrp, {
+        foreignKey: {
+            name: 'user_id',
+            
+            allowNull: false
+        },
+        onDelete: 'CASCADE',
+    });
+
+    User.hasMany(Category, {
+        foreignKey: {
+            name: 'user_id',
+            
+            allowNull: false
+        },
+        onDelete: 'CASCADE',
+    });
+
+    User.hasMany(Transaction, {
+        foreignKey: {
+            name: 'user_id',
+            
+            allowNull: false
+        },
+        onDelete: 'CASCADE',
+    });
+
+    User.hasMany(BudgetCategory, {
+        foreignKey: {
+            name: 'user_id',
+            
+            allowNull: false
+        },
+        onDelete: 'CASCADE',
+    });
+
+// one-to-many relationships in sequelize default to SET NULL ON DELETE
+    Transaction.belongsTo(Account, {
+        foreignKey: {
+            name: 'account_id',
+
+            allowNull: true,
+        }
+    })
+
+    Account.hasMany(Transaction,{
+        foreignKey: {
+            name: 'from_account_id',
+
+            allowNull: true,
+        },
+    })
+
+    Account.hasMany(Transaction,{
+        foreignKey: {
+            name: 'to_account_id',
+
+            allowNull: true,
+        },
+    })
+
+    RecurrTransacGrp.hasMany(Transaction,{
+        foreignKey: {
+            name: 'recurr_id',
+
+            allowNull: true
+        }
+    })
+
+    Category.hasMany(Transaction,{
+        foreignKey: {
+            name: 'category_id',
+
+            allowNull: true
+        }
+    })
+
+    Category.hasMany(BudgetCategory,{
+        foreignKey: {
+            name: 'category_id',
+
+            allowNull: false
+        },
+        onDelete: 'CASCADE'
+    })
+
+    Budget.hasMany(BudgetCategory, {
+        foreignKey: {
+            name: 'budget_id',
+
+            allowNull: false
+        },
+        onDelete: 'CASCADE'      
+    })
+};
+  
+module.exports = setupAssociations;
