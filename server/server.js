04eb827738a36bc863f42fdfbfc21a6606385dcf
ian <ian.sobrecaray@gmail.com>
2024-11-07 01:48:24 +0800

    Updated the import statement to import the renamed sequelize instance

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const { sequelize, connectDB } = require("./config/db");
+const { sequelize, connectDB } = require("./config/connection");
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
 // });
