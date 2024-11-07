4c707bdd2710cba067f8b79a998ff3f716a010a5
ian <ian.sobrecaray@gmail.com>
2024-11-08 03:21:30 +0800

    Changed the script in an attempt to fix errors

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
-npx sequelize-cli db:create 2>&1 | grep -q 'already exists' || exit 0
+# npx sequelize-cli db:create 2>&1 | grep -q 'already exists' || exit 0
 # npx sequelize-cli db:migrate
-exec "$@"
+# exec "$@"
+
+#!/bin/sh
+
+# Check if the database already exists
+if npx sequelize-cli db:create 2>&1 | grep -q 'already exists'; then
+  exit 0
+fi
+
+# If a command is provided, execute it
+if [ $# -gt 0 ]; then
+  exec "$@"
+else
+  echo "No command provided to execute."
+  exit 1
+fi
