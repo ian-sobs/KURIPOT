322cef82e6a5d91d8c9c5122d3ca6e84375a1b60
ian <ian.sobrecaray@gmail.com>
2024-11-08 02:14:40 +0800

    Wrapped RecurrTransacGrp model in a function and defined its associations

--- server/models/RecurrTransacGrp.js
+++ server/models/RecurrTransacGrp.js
@@ -1,55 +1,66 @@
-const {DataTypes, Model } = require('sequelize');
-const {sequelize, connectDB} = require('../config/connection');
+const {Model } = require('sequelize');
+//const {sequelize, connectDB} = require('../config/connection');
 
-class RecurrTransacGrp extends Model{}
+module.exports = (sequelize, DataTypes) => {
+  class RecurrTransacGrp extends Model{
+    static associate(models){
+      RecurrTransacGrp.hasMany(models.Transaction,{
+        foreignKey: {
+            name: 'recurr_id',
 
-RecurrTransacGrp.init(
-  {
-    // Model attributes are defined here
-    id:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    amount: {
-      type: DataTypes.DECIMAL(16,2),
-      defaultValue: 0,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
+            allowNull: true
+        }
+      })    
+      RecurrTransacGrp.belongsTo(models.User,{foreignKey: 'user_id'})
+    }
+  }
 
-      references: {
-        model: User,
-        key: 'id'
+  RecurrTransacGrp.init(
+    {
+      // Model attributes are defined here
+      id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      amount: {
+        type: DataTypes.DECIMAL(16,2),
+        defaultValue: 0,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+
+        // references: {
+        //   model: User,
+        //   key: 'id'
+        // }
+      },
+      startDate:{
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      endDate:{
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      status:{
+        type: DataTypes.ENUM('inactive', 'active'),
+        allowNull: false,
+      },
+      occurDate:{
+        type: DataTypes.JSONB,
+        allowNull: false,
       }
     },
-    startDate:{
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    endDate:{
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    status:{
-      type: DataTypes.ENUM('inactive', 'active'),
-      allowNull: false,
-    },
-    occurDate:{
-      type: DataTypes.JSONB,
-      allowNull: false,
-    }
-  },
-  {
-    // Other model options go here
-    sequelize,
-    modelName: 'RecurrTransacGrp',
-    tableName: 'recurrTransacGrps',
+    {
+      // Other model options go here
+      sequelize,
+      modelName: 'RecurrTransacGrp',
+      tableName: 'recurrTransacGrps',
 
-    // add an option such that when a RecurrTransacGrp is deleted, any future transaction, or all transactions, that references a RecurrTransacGrp record is deleted from the transactions table.
-  },
-);
-
-module.exports = RecurrTransacGrp;
+      // add an option such that when a RecurrTransacGrp is deleted, any future transaction, or all transactions, that references a RecurrTransacGrp record is deleted from the transactions table.
+    },
+  );
+}
