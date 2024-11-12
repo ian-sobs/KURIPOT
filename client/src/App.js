516273473922b8541d7adf51797235e52d489719
ian <ian.sobrecaray@gmail.com>
2024-11-12 23:21:57 +0800

    Non-essential change

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>  kuting, ingna santa claus nag regards ko.
+          Edit <code>src/App.js</code>  kuting, ingna santa claus nag regards ko :D.
         </p>
         <a
           className="App-link"
