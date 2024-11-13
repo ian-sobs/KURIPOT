882af1dd1bbb472f4302ae34e0d4e54acb01a2d4
ian <ian.sobrecaray@gmail.com>
2024-11-13 12:54:28 +0800

    Imported the dotenv package

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 //just to start things up
+require('dotenv').config();
 
 const express = require("express");
 const app = express();
@@ -12,21 +13,26 @@ const registrationRouter = require('./routes/registrationRouter')
 
 // Start the server and connect to the database
 const startServer = async () => {
-  if(process.env.NODE_ENV === "test"){
-    try{
-      await db.sequelize.sync()
-      console.log("Models have been synchronized to the database")
-    } catch (error){
-      console.error("Failed to synchronize models:", error)
-    }
-  }
+
   try {
     await connectDB(); // Initialize the database connection
     console.log("Started the server");
+  
+    // Execute this only after the first try-catch block is successful
+    if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development") {
+      try {
+        await db.sequelize.sync();
+        console.log("Models have been synchronized to the database");
+      } catch (error) {
+        console.error("Failed to synchronize models:", error);
+      }
+    }
   } catch (error) {
+    // This will handle errors from the connectDB or the first block
     console.error("Failed to start server:", error);
     process.exit(1);
   }
+  
   
   app.use(express.json());  // Parses JSON body and adds it to req.body
   app.use(express.urlencoded({ extended: true }));  // Parses form data and adds it to req.body
