b9854860b9473fb77575a2a61708630221630c27
ian <ian.sobrecaray@gmail.com>
2024-11-13 20:00:23 +0800

    Sign API returns a JWT for a successful sign in

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ const db = require('../models/index')
 const sequelize = db.sequelize
 const {User} = sequelize.models
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 
 async function checkIfUserExists(email) {
     const user = await User.findOne({ where: { email } });
@@ -45,35 +46,43 @@ exports.registerUser = async (req,res) =>{
 exports.signInUser = async (req, res) => {
     const {password} = req.body
     const email = req.body.email.trim()
-
-    const userExists = await checkIfUserExists(email)
+    //const userExists = await checkIfUserExists(email)
 
     try {    
         const user = await User.findOne({where:{email: email}})
         
         if (!user) {
             // User already exists, return a conflict response
-            return res.status(409).json({ message: "Incorrect email or password, sign-in failed" });
+            return res.status(401).json({ message: "Incorrect email or password, sign-in failed" });
         }
 
-        const match = await bcrypt.compare(password, user.password)
+        const match = await bcrypt.compare(password, user.password).catch(err => {
+            console.error("Error comparing password:", err);
+            return false;
+          });
 
         if(!match){
             // User already exists, return a conflict response
-            return res.status(409).json({ message: "Incorrect email or password, sign-in failed" });
+            return res.status(401).json({ message: "Incorrect email or password, sign-in failed" });
         }
 
-        
-        console.log("User signed in successfully")      
-        console.log(newUser) 
+        const token = jwt.sign({
+            sub: user.id,                   // User's unique ID
+            username: user.username,         // Username
+            email: user.email,               // Email (optional)
+          }, process.env.JWT_SECRET, {
+            expiresIn: '90m' // 90 minutes
+          });
 
-        return res.status(201).json({
-            message: "User registered successfully",
-            user: newUser // Optional: you can return the newly created user data if needed
+        console.log("User signed-in successfully") 
+
+        return res.status(200).json({
+            message: "User signed-in successfully",
+            jwt: token // Optional: you can return the newly created token if needed
         });
     }
     catch (error) {
-        console.error("Error during user registration:", error);
+        console.error("Error during user sign-in:", error);
         return res.status(500).json({ error: "Internal server error" });
     }
 }
