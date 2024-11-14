bcc300d657a2e3d059a1893a1ce0addb4ebfae9b
ian <ian.sobrecaray@gmail.com>
2024-11-15 01:15:16 +0800

    Specific sections of the code are now in a try-catch block instead of almost the entire function code in a single try-catch block

--- server/controllers/user/registerController.js
+++ server/controllers/user/registerController.js
@@ -10,14 +10,19 @@ exports.registerUser = async (req,res) =>{
     const {username, birthDate, password} = req.body
     const email = req.body.email.trim()
 
-    const userExists = await checkIfUserExists(email)
-
-    try {
+    try{
+        const userExists = await checkIfUserExists(email)
         if (userExists) {
             // User already exists, return a conflict response
             return res.status(409).json({ message: "User already exists, sign-up failed" });
         }
-    
+    }
+    catch(error){
+        console.error("Error during user registration:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+
+    try{
         const newUser = await User.create(
             {
                 username: username, 
@@ -27,25 +32,26 @@ exports.registerUser = async (req,res) =>{
             })
         console.log("User registered successfully")      
         console.log(newUser) 
-
-        const accessToken = makeAccessToken(newUser)
-        const refreshToken = makeRefreshToken(user)
-
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'Strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-        });
-        
-        return res.status(201).json({
-            message: "User registered successfully",
-            user: newUser, // Optional: you can return the newly created user data if needed
-            jwt: accessToken
-        });
     }
-    catch (error) {
+    catch(error){
         console.error("Error during user registration:", error);
-        return res.status(500).json({ error: "Internal server error" });
+        return res.status(500).json({ error: "Internal server error" });        
     }
+
+    const accessToken = makeAccessToken(newUser)
+    const refreshToken = makeRefreshToken(newUser)
+
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'Strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    });
+
+    return res.status(201).json({
+        message: "User registered successfully",
+        user: newUser, // Optional: you can return the newly created user data if needed
+        jwt: accessToken
+    });
+
 }
