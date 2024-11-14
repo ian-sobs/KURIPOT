7f540f1af932332279f3be2a1a62bffa57b4a9e0
chanchan <111845681+ChanAbay-abay@users.noreply.github.com>
2024-11-15 00:19:03 +0800

    Update LandingPage.jsx

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -13,39 +13,39 @@ const LandingPage = () => {
         <div className="slogan-container w-full h-56 bg-center-vertical p-6">
           <div className="slogan-container-content h-full w-full flex items-center justify-between">
             <div className="slogan-container-slogan text-2xl text-[#FAFAFA]">
-              "Its not the amount, <br /> but the habit."
+              "It's not the amount, <br /> but the habit."
             </div>
             <div className="slogan-container-media-placeholder">
               placeholder
             </div>
           </div>
         </div>
-        <div className="carousel-container w-full h-64 flex justify-center overflow-x-auto snap-x snap-mandatory p-4">
-          <div className="carousel-item h-full w-[70%] flex-shrink-0 snap-center flex justify-center items-center m-2">
+        <div className="carousel-container w-full h-64 flex justify-center overflow-x-auto snap-x snap-mandatory p-4 space-x-4">
+          <div className="carousel-item h-full w-[85%] flex-shrink-0 snap-center flex justify-center items-center m-2">
             <img
               src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-              className="rounded-box bg-red-500 h-full"
+              className="rounded-box bg-red-500 h-full w-full object-cover"
               alt="carousel-item"
             />
           </div>
-          <div className="carousel-item h-full w-[70%] flex-shrink-0 snap-center flex justify-center items-center m-2">
+          <div className="carousel-item h-full w-[85%] flex-shrink-0 snap-center flex justify-center items-center m-2">
             <img
               src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-              className="rounded-box bg-red-500 h-full"
+              className="rounded-box bg-red-500 h-full w-full object-cover"
               alt="carousel-item"
             />
           </div>
-          <div className="carousel-item h-full w-[70%] flex-shrink-0 snap-center flex justify-center items-center m-2">
+          <div className="carousel-item h-full w-[85%] flex-shrink-0 snap-center flex justify-center items-center m-2">
             <img
               src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-              className="rounded-box bg-red-500 h-full"
+              className="rounded-box bg-red-500 h-full w-full object-cover"
               alt="carousel-item"
             />
           </div>
-          <div className="carousel-item h-full w-[70%] flex-shrink-0 snap-center flex justify-center items-center m-2">
+          <div className="carousel-item h-full w-[85%] flex-shrink-0 snap-center flex justify-center items-center m-2">
             <img
               src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
-              className="rounded-box bg-red-500 h-full"
+              className="rounded-box bg-red-500 h-full w-full object-cover"
               alt="carousel-item"
             />
           </div>
