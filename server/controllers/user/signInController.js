f5a5ac6e4d81a02ea5cd2ff1029d5359d99fdded
ian <ian.sobrecaray@gmail.com>
2024-11-15 01:28:17 +0800

    functon now has multiple try-catch blocks for specific sections instead of a single-try catch block for the whole code. Secure flag of cookie is true only if the server environment is 'production'

--- server/controllers/user/signInController.js
+++ server/controllers/user/signInController.js
@@ -8,15 +8,22 @@ const makeRefreshToken = require('../utility/makeRefreshToken')
 exports.signInUser = async (req, res) => {
     const {password} = req.body
     const email = req.body.email.trim()
+    let user
 
     try {    
-        const user = await User.findOne({where:{email: email}})
+        user = await User.findOne({where:{email: email}})
         
         if (!user) {
             // User doesn't exist, return a conflict response
             return res.status(401).json({ message: "User bearing that email does not exist" });
         }
+    }
+    catch(error){
+        console.error("Error during user sign-in:", error);
+        return res.status(500).json({ error: "Internal server error" });        
+    }
 
+    try{
         const match = await bcrypt.compare(password, user.password).catch(err => {
             console.error("Error comparing password:", err);
             return false;
@@ -26,26 +33,27 @@ exports.signInUser = async (req, res) => {
             // Wrong password inputted
             return res.status(401).json({ message: "Incorrect email or password, sign-in failed" });
         }
-
-        // jwt automatically adds an 'issuedAt' attribute to the token
-        const accessToken = makeAccessToken(user)
-        const refreshToken = makeRefreshToken(user)
-
-        console.log("User signed-in successfully") 
-        
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'Strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-        });
-        return res.status(200).json({
-            message: "User signed-in successfully",
-            jwt: accessToken // Optional: you can return the newly created token if needed
-        });
     }
-    catch (error) {
+    catch(error){
         console.error("Error during user sign-in:", error);
-        return res.status(500).json({ error: "Internal server error" });
+        return res.status(500).json({ error: "Internal server error" });                
     }
+        // jwt automatically adds an 'issuedAt' attribute to the token
+    const accessToken = makeAccessToken(user)
+    const refreshToken = makeRefreshToken(user)
+
+    console.log("User signed-in successfully") 
+
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'Strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    });
+
+    return res.status(200).json({
+        message: "User signed-in successfully",
+        jwt: accessToken // Optional: you can return the newly created token if needed
+    });
+    
 }
