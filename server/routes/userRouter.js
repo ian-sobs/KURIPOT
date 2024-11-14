77068023c043948b646332821c0b8d2f83f31d02
ian <ian.sobrecaray@gmail.com>
2024-11-15 01:36:24 +0800

    Fixed the issue of router having an object as a parameter instead of a callback

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const registerController = require('../controllers/user/registerController')
-const signInController = require('../controllers/user/signInController')
+const {registerUser} = require('../controllers/user/registerController')
+const {signInUser} = require('../controllers/user/signInController')
 
-router.post('/signUp', registerController)
-router.post('/signIn', signInController)
+router.post('/signUp', registerUser)
+router.post('/signIn', signInUser)
 
 module.exports = router;
