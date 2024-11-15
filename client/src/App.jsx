2c0691de2f4b8205ce31d6eea6f9dde22283ecfc
chanchan <111845681+ChanAbay-abay@users.noreply.github.com>
2024-11-15 23:48:24 +0800

    added: signup frontend files
    Added files for SignUp (Frontend):
    + SignInOutLayout - reduce redundancy for sign in and out
    + SignUpPage - parent and actual page
    + SignUpForm - child, will contain input fields etc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Use Routes instead of Switch
 import LandingPage from "./pages/LandingPage";
+import SignUpPage from "./pages/SignUpPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />{" "}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </Router>
   );
---
+++ client/src/components/SignInOutLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignInOutLayout = () => {
+  return <></>;
+};
+
+export default SignInOutLayout;
