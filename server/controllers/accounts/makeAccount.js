a23e845aae225cabc92474191546ac39e02e3a9f
ian <ian.sobrecaray@gmail.com>
2024-11-22 23:49:36 +0800

    updated: Finished the makeAccount API

--- server/controllers/accounts/makeAccount.js
+++ server/controllers/accounts/makeAccount.js
@@ -4,29 +4,27 @@ const {Account} = sequelize.models
 
 exports.makeAccounts = async (req, res)=>{
     const {userId, usrname, email} = req.user
+    const {name, amount} = req.body
 
-    if (!userId) {
+    if (!name || !userId) {
         return res.status(400).json({ message: 'User ID is required' });
     }
 
-    // let accountsOfUser
-
     try{
-        // accountsOfUser = await Account.findAll({
-        //     where: {
-        //         user_id: userId
-        //     },
-        //     attributes: ['id', 'name', 'amount']
-        // })
-        // res.status(200).json(accountsOfUser); // Send accounts as a response (if in a route handler)
+        const accountOfUser = await Account.create(
+            {
+                name: name, 
+                user_id: userId, 
+                amount: amount || 0
+            }); 
+
+        return res.status(201).json({
+            message: 'Account made successfully',
+            account: accountOfUser
+        })
     } catch (err) {
         console.error('Error making the account:', err.message); // Log the error
-        res.status(500).json({ message: 'Failed to make account' }); // Respond with an error
+        return res.status(500).json({ message: 'Failed to make account' }); // Respond with an error
     }
 
-    // if(!accountsOfUser){
-    //     res.status(404).json({message: "Could not find any account"})
-    // }
-
-    // return res.status(200).json(accountsOfUser)
 }
