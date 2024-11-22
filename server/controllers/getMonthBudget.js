8cf6c23399215da6a82d3ee327ed50faf7465bcd
ian <ian.sobrecaray@gmail.com>
2024-11-22 22:11:18 +0800

    updated: the getMonthBudget API now returns the budget and its associated categories

--- server/controllers/getMonthBudget.js
+++ server/controllers/getMonthBudget.js
@@ -3,6 +3,7 @@ const sequelize = db.sequelize
 const {Budget, Category} = sequelize.models 
 const { Op, Sequelize } = require('sequelize');
 
+
 exports.getMonthBudget = async (req, res)=>{
     const {userId, usrname, email} = req.user
     const {month, year} = req.query
@@ -11,16 +12,24 @@ exports.getMonthBudget = async (req, res)=>{
         return res.status(400).json({ message: 'User ID is required' });
     }
 
-    let budgetMonthOfUser
+    let monthBudget
 
     try{
-        budgetMonthOfUser = await Budget.findOne({
+        monthBudget = await Budget.findOne({
             where: {
                 [Op.and]: [
                     { user_id: userId },  // user_id condition
                     Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('date')), { [Op.eq]: month }),  // Match the month
                     Sequelize.where(Sequelize.fn('YEAR', Sequelize.col('date')), { [Op.eq]: year })   // Match the year
                 ]
+            },
+
+            attributes: ['id', 'date', 'budgetLimit', 'type'],
+            
+            include: {
+                model: Category, // Include the associated B records
+                attributes: ['id', 'name', 'isIncome'],
+                through: { attributes: ['categoryLimit'] } // Optional: Exclude attributes of the through table C
             }
         })
     } catch (err) {
@@ -28,10 +37,13 @@ exports.getMonthBudget = async (req, res)=>{
         res.status(500).json({ message: 'Failed to fetch budget of a month' }); // Respond with an error
     }
 
-    if(!budgetMonthOfUser){
+    if(!monthBudget){
         res.status(404).json({message: 'No such budget exists'})
     }
 
-    
+    return res.status(200).json({
+        message: "Budget and its categories fetched successfully",
+        monthBudget: monthBudget
+    })
 
 }
