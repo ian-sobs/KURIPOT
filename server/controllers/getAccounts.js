6cdd104068df444270b708a58bcfd936707559a1
ian <ian.sobrecaray@gmail.com>
2024-11-22 17:02:57 +0800

    added: API for getting the accounts of a user. User credentials come from the decoded JWT in the authentication middleware

---
+++ server/controllers/getAccounts.js
@@ -0,0 +1,26 @@
+const db = require('../models/index')
+const sequelize = db.sequelize
+const {Account} = sequelize.models 
+
+exports.getAccounts = async (req, res)=>{
+    const {userId, usrname, email} = req.user
+
+    if (!userId) {
+        return res.status(400).json({ message: 'User ID is required' });
+    }
+
+    let accountsOfUser
+
+    try{
+        accountsOfUser = await Account.findAll({
+            where: {
+                user_id: userId
+            }
+        })
+        res.status(200).json(accountsOfUser); // Send accounts as a response (if in a route handler)
+    } catch (err) {
+        console.error('Error fetching accounts:', err.message); // Log the error
+        res.status(500).json({ message: 'Failed to fetch accounts' }); // Respond with an error
+    }
+
+}
