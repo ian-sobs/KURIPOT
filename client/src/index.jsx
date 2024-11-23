afe65468805c4c8971199ad59836559751c674b4
ian <ian.sobrecaray@gmail.com>
2024-11-24 03:18:22 +0800

    updated: import path of TokenContext.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { TokenProvider } from './TokenContext';
+import { TokenProvider } from './data_handling_req/TokenContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
