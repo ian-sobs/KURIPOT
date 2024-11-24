4d7a85e62b6fa2d97975f2cf978dc6e5d7f39951
ian <ian.sobrecaray@gmail.com>
2024-11-25 01:48:00 +0800

    updated: returns custom object structure to client

--- server/controllers/transactions/topMonthSpending.js
+++ server/controllers/transactions/topMonthSpending.js
@@ -33,12 +33,8 @@ exports.topMonthSpending = async (req, res) => {
                 amount: {
                     [Op.lt]: 0
                 },
-                from_account_id: {
-                    [Op.eq]: null // from_account_id is null
-                },
-                to_account_id: {
-                    [Op.eq]: null // to_account_id is null
-                },
+                from_account_id: null, // from_account_id is null
+                to_account_id: null, // to_account_id is null
                 [Op.and]: [
                     Sequelize.where(
                         Sequelize.fn('EXTRACT', Sequelize.literal('MONTH FROM'), Sequelize.col('date')),
@@ -58,8 +54,8 @@ exports.topMonthSpending = async (req, res) => {
                 'category_id',
                 'categoryName', // Group by 'category'
                 [sequelize.fn('sum', sequelize.col('amount')), 'spent'], // Sum of 'amount' for each category
-              ],
-              where: {
+            ],
+            where: {
                 user_id: usrId, // Filter by userId
                 [Op.and]: [
                     Sequelize.where(
@@ -71,11 +67,27 @@ exports.topMonthSpending = async (req, res) => {
                         { [Op.eq]: parsedYear }
                     ),
                 ]
-              },
-              group: ['category_id'], // Group by 'category'
+            },
+            group: ['category_id'], // Group by 'category'
+            order: [sequelize.literal('spent DESC')], // Use a literal for ordering by the alias
+        })
+
+        let catMonthSpend = categorySpending.map((catDetails) => {
+            return {
+                categoryId: catDetails.category_id,
+                categoryName: catDetails.categoryName,
+                spent: catDetails.spent,
+                spentPercentage: ((catDetails.spent / totalSpent) * 100).toFixed(2)
+            }
+        })
+
+        return res.status(200).json({
+            totalSpent: totalSpent,
+            categories: catMonthSpend
         })
     } catch(err){
-
+        console.error('Error fetching transactions:', err); // Log the error
+        return res.status(500).json({ message: 'Failed to fetch top spending categories in a given month of a year' });
     }
 
 }
