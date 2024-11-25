c846e37dd02ddfd644f1886f139b976b19b18277
ian <ian.sobrecaray@gmail.com>
2024-11-25 14:03:02 +0800

    updated: can now return an empty array if no budgets are found

---
+++ server/controllers/budgets/getBudgets.js
@@ -0,0 +1,30 @@
+const db = require('../../models/index')
+const sequelize = db.sequelize
+const {Budget} = sequelize.models 
+
+exports.getAccounts = async (req, res)=>{
+    const {userId, usrname, email} = req.user
+
+    if (!userId) {
+        return res.status(400).json({ message: 'User ID is required' });
+    }
+
+    try{
+        const budgetsOfUser = await Budget.findAll({
+            where: {
+                user_id: userId
+            },
+            attributes: ['id', 'date', 'budgetLimit', 'type']
+        })
+    
+        // if(budgetsOfUser.length === 0){
+        //     res.status(404).json({message: "Could not find any account"})
+        // }
+    
+        return res.status(200).json(budgetsOfUser)
+    } catch (err) {
+        console.error('Error fetching accounts:', err.message); // Log the error
+        return res.status(500).json({ message: 'Failed to fetch accounts' }); // Respond with an error
+    }
+
+}
