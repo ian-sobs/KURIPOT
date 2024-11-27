93557615b46eda01e2712c43a29e8977bb0d56b9
ian <ian.sobrecaray@gmail.com>
2024-11-27 12:09:19 +0800

    updated: getTotalExpense API and getTransactions API are now included in the protected router

--- server/routes/protectedRouter.js
+++ server/routes/protectedRouter.js
@@ -8,7 +8,8 @@ const {getMonthTransac} = require('../controllers/transactions/getMonthTransac')
 const {getRangeTransac} = require('../controllers/transactions/getRangeTransac')
 const {getDayTransac} = require('../controllers/transactions/getDayTransac')
 const {topMonthSpending} = require('../controllers/transactions/topMonthSpending')
-
+const {getTransac} = require('../controllers/transactions/getTransactions')
+const {getTotalExpense} = require('../controllers/transactions/getTotalExpense')
 // Example of a protected route that only authenticated users can access
 protectedRouter.get('/profile', (req, res) => {
   // You can access user data from `req.user` because it's set by the `authenticateToken` middleware
@@ -29,7 +30,8 @@ protectedRouter.get('/getMonthBudgetCategories', getMonthBudgetCategories)
 protectedRouter.get('/getMonthTransac', getMonthTransac)
 protectedRouter.get('/getRangeTransac', getRangeTransac)
 protectedRouter.get('/getDayTransac', getDayTransac)
-
+protectedRouter.get('/getTransactions', getTransac)
 protectedRouter.get('/getTopMonthSpending', topMonthSpending)
+protectedRouter.get('/getTotalExpense', getTotalExpense)
 
 module.exports = protectedRouter;
