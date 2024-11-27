c5bf672900b3aa045635e1fcc6d6a28bfd06f95b
ian <ian.sobrecaray@gmail.com>
2024-11-27 12:13:27 +0800

    created: API controller for getting the total income in a given date or date range'

---
+++ server/controllers/transactions/getTotalIncome.js
@@ -0,0 +1,33 @@
+
+const db = require('../../models/index')
+const {sequelize} = db
+const {Transaction} = sequelize.models
+const {Op, Sequelize, where} = require('sequelize')
+const {valQueryParamDate} = require('../utility/valQueryParamDate')
+
+exports.getTotalIncome = async (req, res) => {
+    const {usrId} = req.user
+
+    let whereClause = valQueryParamDate(req.query, res, 'date')
+
+    whereClause.user_id = usrId
+    whereClause.amount = {
+        [Op.gt]: 0
+    }
+    whereClause.from_account_id = null
+    whereClause.to_account_id = null
+
+    try{
+        const totalEarned = await Transaction.sum('amount', {
+            where: whereClause
+        })
+
+        return res.status(200).json({
+            totalIncome: totalEarned
+        })
+    } catch(err){
+        console.error('Error fetching transactions:', err); // Log the error
+        return res.status(500).json({ message: 'Failed to fetch total income' });
+    }
+
+}
