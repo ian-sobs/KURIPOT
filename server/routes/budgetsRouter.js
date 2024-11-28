5743e193ee88b0cf730f9ff9b2992b9fb3d0a1ee
ian <ian.sobrecaray@gmail.com>
2024-11-28 22:58:32 +0800

    updated: defined the route for the makeBudget API

--- server/routes/budgetsRouter.js
+++ server/routes/budgetsRouter.js
@@ -3,9 +3,12 @@ const budgetsRouter = express.Router();
 const {getMonthBudgetCategories} = require('../controllers/budgets/getMonthBudgetCategories')
 const {getBudgetCategories} = require('../controllers/budgets/getBudgetCategories')
 const {getBudgets} = require('../controllers/budgets/getBudgets')
+const {makeBudget} = require('../controllers/budgets/makeBudget')
 
 budgetsRouter.get('/getMonthBudgetCategories', getMonthBudgetCategories)
 budgetsRouter.get('/getBudgetCategories', getBudgetCategories)
 budgetsRouter.get('/getBudgets', getBudgets)
+
+budgetsRouter.post('/makeBudget', makeBudget)
 
 module.exports = budgetsRouter;
