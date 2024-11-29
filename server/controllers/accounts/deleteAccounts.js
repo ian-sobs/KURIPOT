130cb70dbf5ab9649ad048b904c924c59ef776ef
ian <ian.sobrecaray@gmail.com>
2024-11-29 23:53:22 +0800

    updated: controller accepts an array of account IDs to delete and returns the appropriate message to the client

--- server/controllers/accounts/deleteAccounts.js
+++ server/controllers/accounts/deleteAccounts.js
@@ -3,41 +3,28 @@ const sequelize = db.sequelize
 const {Account} = sequelize.models 
 
 exports.deleteAccounts = async (req, res) => {
-    if(!req.body.id){
-        return res.status(400).json({ message: 'Account ID is required' });
+    if(!req.body.idArr || req.body.idArr.length === 0){
+        return res.status(400).json({ message: 'Account IDs required' });
     }
-
+    const {idArr} = req.body
     const {usrId} = req.user
-    let updateObj = {}
-    let updatedFields = Object.keys(req.body).filter(
-        (field) => field !== 'id' && field !== 'user_id'
-    );
-    updatedFields.forEach(fieldUpdate => {
-        updateObj[fieldUpdate] = req.body[fieldUpdate]
-    });
 
     try {
-        // Change everyone without a last name to "Doe"
-        let [affectedRowsNum, affectedRows] = await Account.update(
-            updateObj,
-            {
-                where: {
-                    id: req.body.id,
-                    user_id: usrId
-                },
-                returning: true
-            },
-        );
+        let message = {}
+        let numDestroyedRows = await Account.destroy({
+            where: {
+                id: idArr,
+                user_id: usrId
+            }
+        })
 
-        if (affectedRowsNum === 0) {
-            return res.status(404).json({ message: 'Account not found or no changes made' });
+        if(numDestroyedRows > 0){
+            return res.status(200).json({message: `Successfully deleted ${numDestroyedRows} account(s).`})
         }
 
-        let [updatedAccount] = affectedRows
-
-        res.status(200).json(updatedAccount)
+        return res.status(404).json({message: 'No accounts found to delete for the specified user.'})
     } catch (error) {
-        console.error('Error updating the account:', error); // Log the error
-        return res.status(500).json({ message: 'Failed to update account' }); // Respond with an error        
+        console.error('Error deleting the accounts:', error); // Log the error for debugging
+        return res.status(500).json({ message: 'Failed to delete account(s), please try again later.' });   
     }
 }
