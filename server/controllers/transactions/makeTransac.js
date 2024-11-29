a5b791eb656c26fb8c96f8cf556b78ada6b9e18e
ian <ian.sobrecaray@gmail.com>
2024-11-29 14:40:50 +0800

    updated: can now handle transfers

--- server/controllers/transactions/makeTransac.js
+++ server/controllers/transactions/makeTransac.js
@@ -165,7 +165,89 @@ async function makeExpense(req, res){
 }
 
 async function makeTransfer(req, res){
+    const {amount, fromAccountId, toAccountId, note} = req.body
+    const {usrId} = req.user
 
+    if(amount < 0){
+        amount = -amount
+    }
+
+    try{
+        let fromAccountInfo = await Account.findOne({
+            where: {
+                id: fromAccountId,
+                user_id: usrId
+            },
+            attributes: ['id', 'name', 'amount']
+        })
+
+        let toAccountInfo = await Account.findOne({
+            where: {
+                id: toAccountId,
+                user_id: usrId
+            },
+            attributes: ['id', 'name', 'amount']
+        })
+
+        if(!fromAccountInfo || !toAccountInfo){
+            return res.status(400).json({message: 'no such account exists'})
+        }
+
+
+        let transacInfo = await Transaction.create({
+            user_id: usrId,
+            amount: amount,
+            account_id: null,
+            accountName: null,
+            date: date,
+            category_id: null,
+            categoryName: null,
+            from_account_id: fromAccountInfo.id,
+            from_accountName: fromAccountInfo.name,
+            to_accountId: toAccountInfo.id,
+            to_accountName: toAccountInfo.name,
+            note: note,
+            recurrId: null
+        })
+
+        await Account.update(
+            { amount: fromAccountInfo.amount - transacInfo.amount },
+            {
+                where: {
+                    id: fromAccountInfo.id,
+                },
+            },
+        );
+
+        await Account.update(
+            { amount: toAccountInfo.amount + transacInfo.amount },
+            {
+                where: {
+                    id: toAccountInfo.id,
+                },
+            },
+        );
+
+        return res.status(201).json({
+            type: 'expense',
+            id: transacInfo.id,
+            date: transacInfo.date,
+            amount: transacInfo.amount,
+            account: {
+                id: transacInfo.account_id,
+                name: transacInfo.accountName
+            },
+            category: {
+                id: transacInfo.category_id,
+                name: transacInfo.categoryName
+            },
+            note: transacInfo.note,
+            recurrId: transacInfo.recurrId
+        })
+    } catch(err){
+        console.error('Error creating the transfer transaction:', err); // Log the error
+        return res.status(500).json({ message: 'Failed to create transfer transaction' });
+    }
 }
 
 exports.makeTransac = async (req, res) => {
