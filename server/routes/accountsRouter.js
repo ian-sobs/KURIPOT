6b0184f9baee9b0cabf44b8bcfc04cc5fb538051
ian <ian.sobrecaray@gmail.com>
2024-11-29 21:35:01 +0800

    updated: defined the API route for the updateAccount controller

--- server/routes/accountsRouter.js
+++ server/routes/accountsRouter.js
@@ -2,8 +2,10 @@ const express = require('express');
 const accountsRouter = express.Router();
 const {getAccounts} = require('../controllers/accounts/getAccounts')
 const {makeAccount} = require('../controllers/accounts/makeAccount')
+const {updateAccount} = require('../controllers/accounts/updateAccount')
 
 accountsRouter.get('/getAccounts', getAccounts)
 accountsRouter.post('/makeAccount', makeAccount)
+accountsRouter.patch('/updateAccount', updateAccount)
 
 module.exports = accountsRouter;
