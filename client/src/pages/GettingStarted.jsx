40d627d0d680d3fa01cd39ad3c9b5de92897d402
chanchan <111845681+ChanAbay-abay@users.noreply.github.com>
2024-12-01 00:50:30 +0800

    added: gettingstarted.jsx
    page is meant for the first sign in of the user where the user will input username, currency, goals (optional), and how did they hear about us.

---
+++ client/src/pages/GettingStarted.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const GettingStarted = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="getting-started-container min-h-screen">
+        getting started
+      </div>
+    </>
+  );
+};
+
+export default GettingStarted;
