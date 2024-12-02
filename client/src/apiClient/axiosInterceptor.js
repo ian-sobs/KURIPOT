d3ae46f1169a37d2e7a50e3b27fa0fc117303898
ian <ian.sobrecaray@gmail.com>
2024-12-02 22:13:36 +0800

    created: request interceptor for protected routes that refreshes the access token

--- client/src/apiClient/axiosInterceptor.js
+++ client/src/apiClient/axiosInterceptor.js
@@ -1,25 +1,61 @@
-import React, { useContext, useEffect } from 'react'
-import { protectedRoute } from './axiosInstance';
+import React, { useContext, useEffect, useCallback } from 'react'
+import { protectedRoute, unprotectedRoute } from './axiosInstance';
 import { TokenContext } from '../token/TokenContext';
+import { useNavigate } from 'react-router-dom';
+import { useJwt } from "react-jwt";
 
 const AxiosRequestInterceptor = ({ children }) => {
     const { accessToken, setAccessToken } = useContext(TokenContext);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const requestInterceptor = protectedRoute.interceptors.request.use(function (config) {
-            // Do something before request is sent
+        console.log('Refreshed access token: ', accessToken)
+    }, [accessToken]);
+
+    const requestInterceptor = useCallback((config) => {
+        const { decodedToken, isExpired } = useJwt(accessToken);
+
+        console.log('ProtectedRoute interceptor');
+
+        if (!decodedToken || isExpired) {
+            console.log('Access token is expired');
+            
+            return unprotectedRoute.get('/token/refresh')
+                .then(function (response) {
+                    // Handle success
+                    console.log('Token refreshed successfully:', response);
+                    const { data } = response;
+                    setAccessToken(data.accessToken);  // Update state with new access token
+
+                    // Return the updated config with new access token in headers
+                    config.headers.Authorization = `Bearer ${data.accessToken}`;
+                    return config;
+                })
+                .catch(function (error) {
+                    console.error('Failed to refresh token:', error);
+
+                    // Redirect to login page and reject request
+                    navigate('/signin');
+                    return Promise.reject('Token refresh failed, redirecting to login.');
+                });
+        } else {
+            // If the token is valid and not expired, attach it to the request headers
+            config.headers.Authorization = `Bearer ${accessToken}`;
             return config;
-          }, function (error) {
-            // Do something with request error
+        }
+    }, [accessToken, navigate, setAccessToken]);
+
+    useEffect(() => {
+        const interceptor = protectedRoute.interceptors.request.use(requestInterceptor, function (error) {
             return Promise.reject(error);
         });
 
         return () => {
-            protectedRoute.interceptors.request.eject(requestInterceptor);
+            protectedRoute.interceptors.request.eject(interceptor);
         }
-    }, [setAccessToken])
+    }, [requestInterceptor]);
 
-    return children
-}
+    return children;
+};
 
-export default AxiosRequestInterceptor
+export default AxiosRequestInterceptor;
