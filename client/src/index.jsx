c9ffb281451f55a96318b7cc05661f6e6719135d
ian <ian.sobrecaray@gmail.com>
2024-12-02 22:15:05 +0800

    updated: made the request interceptor activate for every request to a protected route in the server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { TokenProvider } from './token/TokenContext';
+import AxiosRequestInterceptor from './apiClient/axiosInterceptor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <TokenProvider>
-      <App />
+      <AxiosRequestInterceptor>
+        <App />
+      </AxiosRequestInterceptor>
     </TokenProvider>
   </React.StrictMode>
 );
