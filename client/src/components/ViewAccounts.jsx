f0023545bfbc0a01ce2c5e57b9d78f3092f24d61
elyzuh <katrina.elyza@gmail.com>
2024-12-02 18:01:20 +0800

    updated: ViewAccounts.jsx

--- client/src/components/ViewAccounts.jsx
+++ client/src/components/ViewAccounts.jsx
@@ -7,12 +7,16 @@ const ViewAccounts = () => {
         <div className="flex flex-col h-screen">
             <TaskBar />
             <div className="flex-1 md:ml-[20%] lg:ml-[16.666%]">
-                <PageHeader 
+                <PageHeader
                     title="My Accounts"
                     subtitle="Manage Your Personal Information and Preferences"
-                    onBackClick={() => window.history.back()}/>
+                    onBackClick={() => window.history.back()} />
                 <div className="mt-[4rem] flex items-center justify-center">
-                    <h1 className="text-2xl font-bold text-white">Test View My Accounts isnert add Account</h1>
+                    <h1 className="text-2xl font-bold text-white">Test View My Accounts</h1>
+
+                </div>
+                <div>
+                    <p>this page enables user to view accounts and an option to add an account </p>
                 </div>
             </div>
         </div>
