3b2b5e0728a0ba3e80b928c5278138149a4f7779
chanchan <111845681+ChanAbay-abay@users.noreply.github.com>
2024-12-15 04:59:34 +0800

    fix: budgets overflow problem
    fixed:
    + overflow issue in budgets when components reached >= a certain amount

--- client/src/pages/BudgetsPage.jsx
+++ client/src/pages/BudgetsPage.jsx
@@ -26,46 +26,44 @@ const Budgets = () => {
   }, []);
 
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen">
       <TaskBar />
-      <div className="flex-1 md:ml-[20%] lg:ml-[16.666%] page-with-taskbar">
+      <div className="flex-1 md:ml-[20%] lg:ml-[16.666%] page-with-taskbar overflow-auto">
         <PageHeader
           title="Budgets"
           subtitle="Set a Budget and Stay on Track"
           onBackClick={() => window.history.back()}
         />
-        <div className="page-with-navhead flex justify-center items-center min-h-full p-10">
+        <div className="page-with-navhead min-h-screen p-10">
           {loading ? (
-            // Loading state while data is being fetched
             <div className="flex justify-center items-center h-full text-white">
               Loading...
             </div>
           ) : budgets.length === 0 ? (
-            // If there are no budgets
-            <div className="flex flex-col items-center justify-center text-center mb-8">
-              <img src="/images/budget-box.png" alt="" className="min-h-10" />
-              <div className="text-white text-xl mb-2">
-                You currently have no budget
-              </div>
-              <div className="text-gray-400 text-sm">
-                Input a budget and we'll help you manage your daily income and
-                expenses.
-              </div>
-              <AddBudget />
-              <div className="text-gray-400 text-sm underline">
-                <HowToBudget />
+            <div className="nobudget-container flex justify-center items-center min-h-full">
+              <div className="flex flex-col items-center justify-center text-center mb-8">
+                <img src="/images/budget-box.png" alt="" className="min-h-10" />
+                <div className="text-white text-xl mb-2">
+                  You currently have no budget
+                </div>
+                <div className="text-gray-400 text-sm">
+                  Input a budget and we'll help you manage your daily income and
+                  expenses.
+                </div>
+                <AddBudget />
+                <div className="text-gray-400 text-sm underline">
+                  <HowToBudget />
+                </div>
               </div>
             </div>
           ) : (
-            // If there are budgets
             <div>
               <ul className="text-white">
                 {budgets.map((budget) => (
                   <BudgetCard
-                    key={budget.id} // Pass unique key
+                    key={budget.id}
                     id={budget.id}
-                    budgetLimit={parseFloat(budget.budgetLimit).toFixed(2)} // Ensure budget limit is formatted
-                    //categories={budget.categories || []} // Ensure categories is an array
+                    budgetLimit={parseFloat(budget.budgetLimit).toFixed(2)}
                     date={budget.date}
                     type={budget.type}
                   />
