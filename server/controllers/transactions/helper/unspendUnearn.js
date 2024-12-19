b24846aad0cd2ab74dcea2e0754e19140742b58b
ian <ian.sobrecaray@gmail.com>
2024-12-19 09:45:26 +0800

    updated: updating income and expenses is now functional

--- server/controllers/transactions/helper/unspendUnearn.js
+++ server/controllers/transactions/helper/unspendUnearn.js
@@ -9,22 +9,36 @@ exports.unspendUnearn = async (toUnspendUnearn, usrId) => {
         return null
     }
 
-    if((toUnspendUnearn.amount > 0 && toUnspendUnearn.type === 'expense') || (toUnspendUnearn.amount < 0 && toUnspendUnearn.type === 'income')){
-            toUnspendUnearn.amount = -toUnspendUnearn.amount
+    let deductBy = parseFloat(toUnspendUnearn.amount)
+
+    if((deductBy > 0 && toUnspendUnearn.type === 'expense') || (deductBy < 0 && toUnspendUnearn.type === 'income')){
+            deductBy = -deductBy
     }
 
+    let currentAccount = await Account.findByPk(parseInt(toUnspendUnearn.account_id, 10), {
+        attributes: ['amount']
+    })
+    console.log('unspeadEarn curr account balance', currentAccount.amount)
+    console.log('deductBy', deductBy)
+    let currentAmount = parseFloat(currentAccount.amount)
+    let newBalance = currentAmount - deductBy
+
+    console.log('newBalance', newBalance)
     const [affectedAccountsNum, affectedAccounts] = await Account.update(
         {
-            amount: Sequelize.literal(`amount - ${toUnspendUnearn.amount}`)
+            amount: newBalance
         },
         {
             where: {
                 id: toUnspendUnearn.account_id,
                 user_id: usrId
-            }
+            },
+            returning: true
         }
     );
 
+    console.log("affectedAccountsNum", affectedAccountsNum)
+    console.log("affectedAccounts", affectedAccounts)
     const [affectedAccount] = affectedAccounts
 
     return {
